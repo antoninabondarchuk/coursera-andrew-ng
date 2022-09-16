9e5b105a10de8e993ef5d14f239a67d1011ee41d
Antonina <antonina.bondarchuk1@gmail.com>
2022-09-16 14:53:06 +1000

    Add softmax NN

--- 1 - ML Specialization/1.2 - Advanced Learning Algorithms/nn_basics.py
+++ 1 - ML Specialization/1.2 - Advanced Learning Algorithms/nn_basics.py
@@ -17,6 +17,17 @@ def my_dense(a_in, W, b, g):
     return a_out
 
 
+def my_softmax(z):
+    """ Softmax converts a vector of values to a probability distribution.
+    Args:
+      z (ndarray (N,))  : input data, N features
+    Returns:
+      a (ndarray (N,))  : softmax of z
+    """
+    a = np.exp(z) / np.sum(np.exp(z))
+    return a
+
+
 model = tf.keras.models.Sequential(
     [
         tf.keras.Input(shape=(400,)),  # specify input size
@@ -26,3 +37,15 @@ model = tf.keras.models.Sequential(
     ], name="my_model"
 )
 
+softmax_model = tf.keras.models.Sequential(
+    [
+        tf.keras.Input(shape=(400,)),
+        tf.keras.layers.Dense(units=25, activation='relu'),
+        tf.keras.layers.Dense(units=15, activation='relu'),
+        tf.keras.layers.Dense(units=10, activation='linear')
+    ], name="my_softmax_model"
+)
+softmax_model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+)
